4601eb1e9b9853e3e1880b14459bd294ddcbbaa5
SecurityQQ <flesh.game@mail.ru>
2017-10-16 23:02:49 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from telegram.ext import Updater, MessageHandler, BaseFilter
+
+from smart_qr_codes import smart_qr_code_by_name
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.ERROR)
+
+
+def start(bot, update):
+    update.message.reply_text("Hello! I'm a very creative bot! I do QRCodes from your content and description. Just send me a link and a name of object you want to display on your QR-Code")
+
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+updater = Updater('414610099:AAHG4Mf5lw05PxI4JY9ZP6CpTlkauCk4AAo')
+
+class IsCorrect(BaseFilter):
+    def filter(self, message):
+        return True
+
+def do_qr_code(bot, update):
+    splitted_text = update.message.text.split()
+    content = splitted_text[0]
+    name = " ".join(splitted_text[1:])
+
+    def send_qr(qr):
+        bot.send_photo(update.message.chat_id,
+                       photo=qr,
+                       caption=name)
+
+
+    smart_qr_code_by_name(content, name, suffix=".png", save_name=None, hook=send_qr)
+
+
+isCorrectFilter = IsCorrect(
+    
+
+echo_handler = MessageHandler(isCorrectFilter, do_qr_code)
+updater.dispatcher.add_handler(echo_handler)
+updater.start_polling()
+updater.idle()
---
+++ smart_qr_codes.py
@@ -0,0 +1,48 @@
+from qrcode.MyQR import myqr
+
+
+import requests as r
+import random
+import os
+
+
+def smart_qr_code_by_name(content, seed, suffix=".png", save_name=None, hook=lambda x: x):
+    if save_name is None:
+        save_name = seed + str(random.randint(1, 100000)) + suffix
+
+    if 'icon' not in seed:
+        seed = seed + ' icon' + ' ' + suffix
+
+    name = seed
+
+    print("name: ", name)
+
+    s = r.get("https://api.cognitive.microsoft.com/bing/v7.0/images/search/?q={}".format(name),
+              headers={"Ocp-Apim-Subscription-Key": "63b3ee16a46847e0be92920dd1409024"})
+
+    from pprint import pprint
+    import json
+
+    url = json.loads(s.content).get('value')[0].get('contentUrl')
+
+    print('url: ', url)
+    import tempfile
+    with open("tmp"+suffix, 'wb+') as fp:
+        fp.write(r.get(url).content)
+        path = fp.name
+        print(path)
+        try:
+            ver, ecl, qr_name = myqr.run(content, picture=path, colorized=True, save_name=save_name)
+        except:
+            ver, ecl, qr_name = myqr.run(content, picture=path, colorized=False, save_name=save_name)
+        with open(save_name, 'rb') as output:
+            hook(output)
+        os.remove(save_name)
+        return fp
+
+
+
+if __name__ == '__main__':
+    ver, ecl, qr_name = myqr.run("http://hackupc.com/", picture="/Users/Security/Downloads/120575-200.png", contrast=1.,save_name='pen.png')
+
+# ver, ecl, qr_name = myqr.run("http://goatse.ru/", picture="/Developer/PycharmProjects/WhereToGo/dick.jpg", colorized=False, save_name='dick_qr.jpg')
